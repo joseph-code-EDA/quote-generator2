550c6188180972775896f915616140d6e67b0470
joseph <joseph>
2016-02-24 02:09:23 +1300

    a

--- quotegenerator.js
+++ quotegenerator.js
@@ -62,56 +62,9 @@ function press() {
 		document.getElementById('twitterbutton').innerHTML = "";
 		twttr.widgets.createShareButton('http://tony-luisi.github.io/quote-generator/',document.getElementById('twitterbutton'),{ text: currentQuote, size: 'large' });
 
-
-	});
-
+});
 
 
-var audio1 = document.getElementById("potato");
-
-
-function burn1() {
-    audio1.play();
-}
-
-function burn2() {
-    audio1.play();
-}
-
-function burn3() {
-    audio1.play();
-}
-
-function burn4() {
-    audio1.play();
-}
-
-function burn5() {
-    audio1.play();
-}
-
-function random() {
-    var rand = Math.floor(Math.random() * (5 - 1)) + 1;
-    switch(rand) {
-
-        case 1:
-            burn1();
-            break;
-        case 2:
-            burn2();
-            break;
-        case 3:
-            burn3();
-            break;
-        case 4:
-            burn4();
-
-/*function randomAdLib(){
-	var adLibs = ["./sounds/bl1.mp3", "./sounds/bl2.mp3", "./sounds/bl3.mp3"];
-	$("#element").html("<embed src=\"" + Math.floor (Math.random() * (adLibs.length+1))+"\" autostart=\"true\"/>");
-	//console.log(adLibs);
-}
-	/*<p> <button onclick="randomAdLib()">Try it</button> </p>*/
 
 
 
